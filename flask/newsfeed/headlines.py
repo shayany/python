fdc7ceb03d84da67195d1f37eba3f56f2a287f55
Shayan Yazdanmehr <shayany@ifi.uio.no>
2016-05-06 14:23:20 +0200

    Show all articles

--- flask/newsfeed/headlines.py
+++ flask/newsfeed/headlines.py
@@ -11,10 +11,7 @@ RSS_FEED = {'bbc':"http://feeds.bbci.co.uk/news/world/rss.xml",
 @app.route("/")
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEED[publication])
-    first_article=feed['entries'][0]
-    return  render_template("home.html",title=first_article.get("title"),
-                            published=first_article.get("published"),
-                            summary=first_article.get("summary"))
+    return  render_template("home.html",articles=feed['entries'])
 
 if __name__=="__main__":
     app.run(port=5000,debug=True)
