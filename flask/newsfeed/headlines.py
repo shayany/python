b2ffcda17a562d4feaa82a93869942dd2a4efb4f
Shayan Yazdanmehr <shayany@ifi.uio.no>
2016-05-06 14:12:19 +0200

    Adding Jinja

--- flask/newsfeed/headlines.py
+++ flask/newsfeed/headlines.py
@@ -1,4 +1,4 @@
-from flask import  Flask
+from flask import  Flask,render_template
 import  feedparser
 
 app=Flask(__name__)
@@ -12,16 +12,9 @@ RSS_FEED = {'bbc':"http://feeds.bbci.co.uk/news/world/rss.xml",
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEED[publication])
     first_article=feed['entries'][0]
-    return """
-    <html>
-        <body>
-            <h1> BBD Headline</h1>
-            <b>{0}</b> </br>
-            <i>{1}</i> </br>
-            <p>{2}</p> </br>
-        </body>
-    </html>
-    """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+    return  render_template("home.html",title=first_article.get("title"),
+                            published=first_article.get("published"),
+                            summary=first_article.get("summary"))
 
 if __name__=="__main__":
     app.run(port=5000,debug=True)
